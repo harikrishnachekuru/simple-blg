90b1ff9cb4a26b26c1229ab4298cdd3a8eae0a03
unknown <chekuruharikrishna@gmail.com>
2020-05-09 16:04:43 +0530

    second-commit

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const config = require('./config/key');
 
-mongoose.connect('mongodb+srv://blog:12345@blog-v3gjl.mongodb.net/test?retryWrites=true&w=majority',
+const { User } = require('./models/user');
+
+mongoose.connect(config.mongoURI,
     { useNewUrlParser: true }).then(() => console.log("DB CONNECTED"))
     .catch(err => console.error(err));
 
-app.get('/', (req, res) => {
-    res.send("hello Harry")
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
+app.get('/', (req, res) => {
+    res.json({ "Hello-ra": "~ How-r-u :)" })
+})
+
+app.post('/api/users/register', (req, res) => {
+    const user = new User(req.body)
+
+    user.save((err, userData) => {
+        if (err) return res.json({ success: false, err });
+
+        res.status(200).json({
+            success: true,
+            userData: doc
+        })
+    })
+
+})
 
 app.listen(5000);
 
