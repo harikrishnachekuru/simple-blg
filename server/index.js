9e045c82939ff0c32feba789d913548fd7f41f8b
Hari <chekuruharikrishna@gmail.com>
2020-05-11 18:42:53 +0530

    heroku again

--- server/index.js
+++ server/index.js
@@ -16,6 +16,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.get('/', (req, res) => {
+    res.json({"Hey harry": "it is your first deployment successfully"})
+})
+
+
 app.get('/api/user/auth', auth, (req, res) => {
     res.status(200).json({
         _id: req._id,
